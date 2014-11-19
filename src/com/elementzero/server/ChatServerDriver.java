a5fd8d549469ae0f85bb7d6c44e4546e4ddfdee9
tjzwieg <tjzwieg@stthomas.edu>
2014-11-18 20:44:56 -0600

    updated todo

--- src/com/elementzero/server/ChatServerDriver.java
+++ src/com/elementzero/server/ChatServerDriver.java
@@ -2,6 +2,11 @@ package com.elementzero.server;
 
 public class ChatServerDriver {
 
+	
+	//TODO connection
+	//TODO public key storage
+	//TODO authentication
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
