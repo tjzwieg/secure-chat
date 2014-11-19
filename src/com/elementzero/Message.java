0f18a6246c08ade54cd735cb1d66f3b0d2d8057e
tjzwieg <tjzwieg@stthomas.edu>
2014-11-18 20:41:06 -0600

    Added Message and ChatServerDriver classes

---
+++ src/com/elementzero/Message.java
@@ -0,0 +1,54 @@
+package com.elementzero;
+
+public class Message {
+
+	private Long id;
+	private Object message;
+	private Long toId;
+	private Long fromId;
+	
+	private Message() {}
+	
+	public Message(Long mid, Object message, Long toId, Long fromId) {
+		this.id = mid;
+		this.message = message;
+		this.toId = toId;
+		this.fromId = fromId;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Object getMessage() {
+		return message;
+	}
+
+	public void setMessage(Object message) {
+		this.message = message;
+	}
+
+	public Long getToId() {
+		return toId;
+	}
+
+	public void setToId(Long toId) {
+		this.toId = toId;
+	}
+
+	public Long getFromId() {
+		return fromId;
+	}
+
+	public void setFromId(Long fromId) {
+		this.fromId = fromId;
+	}
+
+	
+	
+	
+}
---
+++ src/com/elementzero/server/ChatServerDriver.java
@@ -0,0 +1,10 @@
+package com.elementzero.server;
+
+public class ChatServerDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
